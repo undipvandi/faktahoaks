0e3d2ddb85ffc98cf4e8737c3e934083eff32246
Devandi Kurnia Restu Andika <vandi@students.undip.ac.id>
2025-05-07 18:47:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,20 +3,21 @@ import openai
 from openai import OpenAI
 
 # Judul aplikasi
-st.title("Deteksi Berita Hoaks")
-st.write("Masukkan teks berita untuk menganalisis apakah berita tersebut berpotensi hoaks atau tidak.")
+st.title("📰 Evaluasi Berita")
+st.subheader("Tugas Transformasi Digital")
+st.write("Aplikasi evaluasi berita fakta dan hoaks dengan kecerdasan buatan")
 
 # Form input teks
 with st.form("hoax_form"):
-    news_text = st.text_area("Teks Berita", height=200, placeholder="Masukkan teks berita di sini...")
-    submit_button = st.form_submit_button("Analisis")
+    news_text = st.text_area("Masukkan teks berita untuk menganalisis apakah berita tersebut berpotensi hoaks atau tidak", height=200, placeholder="Masukkan teks berita di sini...")
+    submit_button = st.form_submit_button("Evaluasi")
 
 # Fungsi untuk memanggil OpenAI API
 def analyze_news(news_text):
     try:
         client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"], base_url="https://api.deepseek.com")  # Ambil API key dari secrets
         prompt = (
-            "Analisis teks berita berikut dan tentukan apakah berita ini berpotensi hoaks atau tidak. "
+            "Analisis dan evaluasi teks berita berikut dan tentukan apakah berita ini berpotensi hoaks atau tidak. "
             "Berikan penjelasan singkat tentang alasan Anda. Jika teks tidak cukup jelas, nyatakan bahwa informasi tambahan diperlukan. "
             f"Teks berita: {news_text}"
         )
@@ -27,6 +28,7 @@ def analyze_news(news_text):
                 {"role": "system", "content": "Anda adalah asisten yang ahli dalam mendeteksi berita hoaks."},
                 {"role": "user", "content": prompt}
             ],
+            temperature=1.0
         )
 
         return response.choices[0].message.content
@@ -42,3 +44,10 @@ if submit_button:
             result = analyze_news(news_text)
             st.subheader("Hasil Analisis")
             st.write(result)
+
+footer_html = """
+<div style='text-align: center;'>
+<p>Dikembangan oleh Kelompok 2 ❤️</p>
+</div>
+"""
+st.markdown(footer_html, unsafe_allow_html=True)
